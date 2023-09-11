57e89e48baccf1c17b815aaede1f6580a172cb95
krllz <keroch66@gmail.com>
2023-09-11 10:49:51 +0800

    Второй

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,10 +1,2 @@
-﻿internal class Program
-{
-    private static void Main(string[] args)
-    {
-        Console.WriteLine("Hello, World!");
-        int x = 0;
-        int y = 6;
-        Console.WriteLine(x - 6);
-    }
-}
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
