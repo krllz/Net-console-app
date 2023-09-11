614a807a382062b580a4b3125e6ce4fbc2ea0470
krllz <keroch66@gmail.com>
2023-09-11 10:34:43 +0800

    Тест

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        int x = 0;
+        int y = 6;
+        Console.WriteLine(x - 6);
+    }
+}
